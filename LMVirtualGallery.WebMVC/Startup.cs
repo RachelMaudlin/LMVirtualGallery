8223f062c71cb603d17422e8ff7a64e7d7d0a9c3
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-05-09 19:54:27 -0400

    Add project files.

---
+++ LMVirtualGallery.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMVirtualGallery.WebMVC.Startup))]
+namespace LMVirtualGallery.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
