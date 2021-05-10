b5227596b054133c2ab71d7d260692db4622cc12
RachelMaudlin <73568853+RachelMaudlin@users.noreply.github.com>
2021-05-09 20:27:41 -0400

    added classes

---
+++ LMVirtualGallery.Data/Composition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMVirtualGallery.Data
+{
+    public class Composition
+    {
+        [Required]
+        [ForeignKey(nameof(Exhibition))]
+        public int ExhibitionId { get; set; }
+
+        [Key]
+        public int CompositionId { get; set; }
+
+        [Required]
+        [Display(Name = "Name of Composition")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string CompositionName { get; set; }
+
+        [Required]
+        public string ImageName { get; set; }
+
+        [Display(Name = "Composition Description")]
+        public string CompositionDescription { get; set; }
+
+        [Display(Name = "Composition Medium")]
+        public string CompositionMedium { get; set; }
+
+        [Display(Name = "Date of Composition Creation")]
+        public string CompositionCreation { get; set; }
+    }
+}
---
+++ LMVirtualGallery.Data/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMVirtualGallery.Data
+{
+    public class Event
+    {
+        public class UpcomingEvents
+        {
+            [Key]
+            public int EventId { get; set; }
+            [Required]
+            [Display(Name = "Name of Event")]
+            public string NameOfEvent { get; set; }
+            [Required]
+            [Display(Name = "Event Description")]
+            public string EventDescription { get; set; }
+            [Required]
+            [Display(Name = "Event Address")]
+            public string EventAddress { get; set; }
+            [Required]
+            [Display(Name = "Event Date")]
+            public DateTime EventDate { get; set; }
+
+        }
+    }
+}
